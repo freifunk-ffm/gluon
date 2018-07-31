0617684cadf885c501023fb8ab4725e25b585f0e
JuS <sagurna@s3i.de>
2018-07-31 16:22:12 +0200

    Es gibt keinen gepflegten Gluon-Master in Frankfurt. Daher -> "Weg damit."

--- scripts/modules.sh
+++
@@ -1,8 +0,0 @@
-. "$GLUONDIR"/modules
-[ ! -f "$GLUON_SITEDIR"/modules ] || . "$GLUON_SITEDIR"/modules
-
-GLUON_MODULES=openwrt
-
-for feed in $GLUON_SITE_FEEDS $GLUON_FEEDS; do
-	GLUON_MODULES="$GLUON_MODULES packages/$feed"
-done
--- scripts/site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-SITE_CONFIG_LUA=scripts/site_config.lua
-
-"$GLUONDIR"/openwrt/staging_dir/host/bin/lua -e "print(assert(dofile(os.getenv('GLUONDIR') .. '/${SITE_CONFIG_LUA}').$1))" 2>/dev/null
